5a5982189c3b9215c686cd53e8f46f15c18d5e97
cheabiiin <ravis_toy@naver.com>
2020-06-02 00:11:06 +0900

    20-06-01 HOME

---
+++ JavaExProject/src/button/ButtonMain.java
@@ -0,0 +1,23 @@
+package button;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class ButtonMain extends Application {
+
+	@Override
+	public void start(Stage primaryStage) throws Exception {
+		Parent root = (Parent)FXMLLoader.load(getClass().getResource("ButtonControl.fxml"));
+		Scene scene = new Scene(root);	
+		
+		primaryStage.setTitle("AppMain");
+		primaryStage.setScene(scene);
+		primaryStage.show();
+	}
+	public static void main(String[] args) {
+		launch(args);
+	}
+}
